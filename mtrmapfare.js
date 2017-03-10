78a17f8f54065bc9a2a732d576e62ad9726576ad
jsp <jsp.eds.com@gmail.com>
2017-03-10 23:10:52 +0800

    remove unused tag and use arrow function

--- mtrmapfare.js
+++ mtrmapfare.js
@@ -7,78 +7,61 @@
  * aesa - airport express station array
  * aeflag - 1 : display ae fare, 0 : display mtr fare
  */
-(function(){
-	var sn="2 111 28 1 37 96 18 12 98 11 55 80 74 69 30 82 51 36 101 39 64 4 115 83 13 8 40 22 15 57 19 21 38 76 9 78 117 102 20 65 6 53 14 31 41 52 16 32 70 34 81 97 68 17 35 7 99 75 26 119 54 33 72 100 67 24 73 29 118 49 50 3 42 114 25 120 43 71 27 10 103 5 48 116 23 84 85 86 87 88 89";
-	var aesn="44 45 46 47 56";
+(()=>{
+	let sn='2 111 28 1 37 96 18 12 98 11 55 80 74 69 30 82 51 36 101 39 64 4 115 83 13 8 40 22 15 57 19 21 38 76 9 78 117 102 20 65 6 53 14 31 41 52 16 32 70 34 81 97 68 17 35 7 99 75 26 119 54 33 72 100 67 24 73 29 118 49 50 3 42 114 25 120 43 71 27 10 103 5 48 116 23 84 85 86 87 88 89'
+	let aesn='44 45 46 47 56'
 // from station default to sn: 73
-	ns.fs=73;
-	ns.ft=0;
-	ns.sa=sn.split(" ");
-	ns.aesa=aesn.split(" ");
-	ns.sa_length=ns.sa.length;
-	ns.aesa_length=ns.aesa.length;
-
-	ns.sf=function(fromstation){
-		this.aeflag=0;
-		this.fs=fromstation;
-		document.getElementById(47).innerHTML='na';
-		document.getElementById(56).innerHTML='na';
-		for (var i=0,j=this.sa_length;i<j;i++){
-			var f=fromstation;
-			var t=this.sa[i];
-			if (t==f){
-				document.getElementById(t).innerHTML="x";
-			} else {
-				if (f>t){var tmp=t;t=f;f=tmp;}
-				t-=f;
-				if (this.ff[f][t] != undefined){
-					document.getElementById(this.sa[i]).innerHTML=this.ff[f][t].split(" ")[this.ft];
+	ns.fs=73
+	ns.ft=0
+	ns.sa=sn.split(' ')
+	ns.aesa=aesn.split(' ')
+	ns.sf=fromstation=>{
+		ns.aeflag=0
+		ns.fs=fromstation
+		document.getElementById(47).innerHTML='na'
+		document.getElementById(56).innerHTML='na'
+    ns.sa.forEach(x=>{
+			let f=fromstation,t=x
+			if (t!=f){
+				if (f>t){let tmp=t;t=f;f=tmp}
+				t-=f
+				if (ns.ff[f][t]!=undefined){
+					document.getElementById(x).innerHTML=ns.ff[f][t].split(' ')[ns.ft]
 				}
-			}
-		}
-	}
-
-	ns.saef=function(fromstation){
-		this.aeflag=1;
-		this.fs=fromstation;
-		for (var i=0,j=this.sa_length;i<j;i++){
-			document.getElementById(this.sa[i]).innerHTML="na";
-		}
-		for (var i=0,j=this.aesa_length;i<j;i++){
-			var f=fromstation;
-			var t=this.aesa[i];
-			if (t==f){
-				var ts=t;
-				switch (ts){
-					case '46': ts=42; break;
-					case '45': ts=40; break;
-					case '44': ts=39;
-				};
-				document.getElementById(ts).innerHTML="x";
 			} else {
-				if (f>t){var tmp=t;t=f;f=tmp;}
-				t-=f;
-				if (this.ff[f][t] != undefined){
-					var ts=this.aesa[i];
-					switch (ts){
-						case '46': ts=42; break;
-						case '45': ts=40; break;
-						case '44': ts=39;
-					};
-					if ((this.ft==1)||(this.ft==4)||(this.ft==5)||(this.ft==7)){
-						document.getElementById(ts).innerHTML="na";
-					} else {
-						document.getElementById(ts).innerHTML=this.ff[f][t].split(" ")[this.ft];
+				document.getElementById(x).innerHTML='x'
+			}
+		})
+	}
+	ns.saef=fromstation=>{
+		ns.aeflag=1
+		ns.fs=fromstation
+    ns.sa.forEach(x=>{document.getElementById(x).innerHTML='na'})
+    ns.aesa.forEach(x=>{
+			let f=fromstation,t=x,ts=x
+			switch (ts){
+				case '46': ts=42; break;
+				case '45': ts=40; break;
+				case '44': ts=39;
+			}
+			if (t!=f){
+				if ((ns.ft==1)||(ns.ft==4)||(ns.ft==5)||(ns.ft==7)){
+					document.getElementById(ts).innerHTML='na'
+				} else {
+				  if (f>t){let tmp=t;t=f;f=tmp}
+  				t-=f
+  				if (ns.ff[f][t]!=undefined){
+						document.getElementById(ts).innerHTML=ns.ff[f][t].split(' ')[ns.ft]
 					}
 				}
+			} else {
+				document.getElementById(ts).innerHTML='x'
 			}
-		}
+		})
 	}
-
-	ns.setft=function(sel){
-		this.ft=parseInt(sel.value);
-		this.aeflag ? this.saef(this.fs) : this.sf(this.fs);
+	ns.setft=sel=>{
+		ns.ft=parseInt(sel.value)
+		ns.aeflag?ns.saef(ns.fs):ns.sf(ns.fs)
 	}
-
-	ns.sf(ns.fs);
-})();
+	ns.sf(ns.fs)
+})()
